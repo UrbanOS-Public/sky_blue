8b51000c0a8bd74c5f2d153839e465731b3c2e60
Marko Ristic <marko.ristic@avanade.com>
2023-05-15 11:24:11 -0400

    rg

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.0.2"
+    }
+  }
+  cloud {
+    organization = "CloudLabM"
+    workspaces {
+      name = "tf-state"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.2.0"
+  suffix  = [var.suffix]
+}
+
+resource "azurerm_resource_group" "urbanos" {
+  name     = module.naming.resource_group.name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "urbanos" {
+  name                      = module.naming.storage_account.name_unique
+  resource_group_name       = azurerm_resource_group.tfstate.name
+  location                  = azurerm_resource_group.tfstate.location
+  account_tier              = "Standard"
+  account_replication_type  = var.account_replication_type
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+
+  infrastructure_encryption_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_storage_container" "urbanos" {
+  name                  = module.naming.storage_container.name
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "suffix" {
+  type = string
+  default = "urbanOS"
+}
+
+variable "account_replication_type" {
+  type = string
+  default = "LRS"
+}
+
+variable "location" {
+  type = string
+  default = "EastUS"
+}
