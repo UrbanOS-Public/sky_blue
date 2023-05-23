8a7396be1334bebcf62a1e9b7978cf5a4f6dc16b
Marko Ristic <marko.ristic@avanade.com>
2023-05-23 14:12:48 -0400

    Azure infra

--- 02_ACR/data.tf
+++ 02_ACR/data.tf
@@ -4,8 +4,7 @@ data "azurerm_resources" "key_vault" {
     name = module.namespoke.key_vault.name
 }
 
-data "azurerm_resources" "log_analytics_workspace" {
-    #type = "Microsoft.KeyVault"
-    resource_group_name = module.namehub.resource_group.name
-    name = module.namehub.log_analytics_workspace.name
+data "azurerm_log_analytics_workspace" "law" {
+  name                = module.namehub.log_analytics_workspace.name
+  resource_group_name = module.namehub.resource_group.name
 }
