c6a0429c39a6de72d5897dcc1ab30a69c37bdb15
Marko Ristic <marko.ristic@avanade.com>
2023-06-01 12:37:16 -0400

    m

--- 04_Jumpbox/modules/virtual_machine/main.tf
+++ 04_Jumpbox/modules/virtual_machine/main.tf
@@ -20,18 +20,6 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = var.resource_group_name
   tags                = var.tags
 
-  dynamic "security_rule" {
-    for_each = { for sg in var.security_rules : sg.name => sg } 
-      name                       = each.key
-      priority                   = each.value.priority
-      direction                  = each.value.direction
-      access                     = each.value.access
-      protocol                   = each.value.protocol
-      source_port_range          = each.value.source_port_range
-      destination_port_range     = each.value.destination_port_range
-      source_address_prefix      = each.value.source_address_prefix
-      destination_address_prefix = each.value.destination_address_prefix
-  }
 
   lifecycle {
    ignore_changes = [
@@ -39,6 +27,27 @@ resource "azurerm_network_security_group" "nsg" {
     ]
   }
 }
+
+resource "azurerm_network_security_rule" "rules" {
+  for_each = { for sg in var.security_rules : sg.name => sg } 
+  
+  name                        = each.key
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  
+  depends_on = [ 
+    azurerm_network_security_group.nsg 
+  ]
+}
+
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.name}-nic"
