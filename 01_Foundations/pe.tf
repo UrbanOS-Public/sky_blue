3f15d7fef3fc365d7a7ccb4840710eadd0e7b7e7
Marko Ristic <marko.ristic@avanade.com>
2023-06-06 08:47:49 -0400

    Azure

--- 01_Foundations/pe.tf
+++ 01_Foundations/pe.tf
@@ -4,9 +4,9 @@ module "key_vault_private_endpoint" {
   name                           = "${(module.namespoke.key_vault.name)}-pe" #"${title(module.key_vault.name)}-pe"
   location                       = var.location
   resource_group_name            = module.namespoke.resource_group.name
-  subnet_id                      = data.azurerm_subnet.spoke_vm_subnet.id #module.spoke_network.subnet_ids[var.vm_subnet_name]
+  subnet_id                      = module.spoke_network.subnet_ids[var.vm_subnet_name] #data.azurerm_subnet.spoke_vm_subnet.id
   tags                           = var.tags
-  private_connection_resource_id = data.azurerm_resources.key_vault.resources[0].id #module.key_vault.id
+  private_connection_resource_id = module.key_vault.id #data.azurerm_resources.key_vault.resources[0].id #
   is_manual_connection           = false
   subresource_name               = "vault"
   private_dns_zone_group_name    = "KeyVaultPrivateDnsZoneGroup"
@@ -18,9 +18,9 @@ module "blob_private_endpoint" {
   name                           = "${(module.namespoke.storage_account.name)}-pe"
   location                       = var.location
   resource_group_name            = module.namespoke.resource_group.name
-  subnet_id                      = data.azurerm_subnet.spoke_vm_subnet.id #module.spoke_network.subnet_ids[var.vm_subnet_name]
+  subnet_id                      = module.spoke_network.subnet_ids[var.vm_subnet_name] #data.azurerm_subnet.spoke_vm_subnet.id #
   tags                           = var.tags
-  private_connection_resource_id = data.azurerm_storage_account.st.id #module.storage_account.id
+  private_connection_resource_id = module.storage_account.id #data.azurerm_storage_account.st.id #
   is_manual_connection           = false
   subresource_name               = "blob"
   private_dns_zone_group_name    = "BlobPrivateDnsZoneGroup"
