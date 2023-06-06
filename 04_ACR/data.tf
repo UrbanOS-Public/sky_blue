6862167d2d118f2ffc84d92f883eb471a4f384e4
Marko Ristic <marko.ristic@avanade.com>
2023-06-06 09:27:12 -0400

    Azure

--- 04_ACR/data.tf
+++ 04_ACR/data.tf
@@ -8,3 +8,9 @@ data "azurerm_log_analytics_workspace" "law" {
   name                = module.namelaw.log_analytics_workspace.name
   resource_group_name = module.namelaw.resource_group.name
 }
+
+data "azurerm_subnet" "spoke_vm_subnet" {
+  name                 = var.vm_subnet_name
+  virtual_network_name = module.namespoke.virtual_network.name
+  resource_group_name  = module.namespoke.resource_group.name
+}
