9b23a9ca42f46a6d723c84e5fdd8022007cf3b80
Marko Ristic <marko.ristic@avanade.com>
2023-06-06 13:32:06 -0400

    Azure

--- scripts/azurepreview.sh
+++ scripts/azurepreview.sh
@@ -1,38 +1,39 @@
-#AKS preview features are available on a self-service, opt-in basis. 
-#Previews are provided "as is" and "as available," and they're excluded from the service-level agreements and limited warranty. 
-#AKS previews are partially covered by customer support on a best-effort basis. As such, these features aren't meant for production use. 
-#For more information, see the following support articles:
-#https://learn.microsoft.com/en-us/azure/aks/vertical-pod-autoscaler
-#https://learn.microsoft.com/en-us/azure/aks/image-cleaner?tabs=azure-cli
-
-#To install the aks-preview extension, run the following command: Azure CLI
-#Open Cloudshell
-#az extension add --name aks-preview
-#Run the following command to update to the latest version of the extension released:
-#az extension update --name aks-preview
-
-#Register the 'AKS-VPAPreview' feature flag
-#Register the AKS-VPAPreview feature flag by using the az feature register command, as shown in the following example:
-#az feature register --namespace "Microsoft.ContainerService" --name "AKS-VPAPreview"
-#It takes a few minutes for the status to show Registered. Verify the registration status by using the az feature show command:
-#az feature show --namespace "Microsoft.ContainerService" --name "AKS-VPAPreview"
-#When the status reflects Registered, refresh the registration of the Microsoft.ContainerService resource provider by using the az provider register command:
-#az provider register --namespace Microsoft.ContainerService
-
-# Use Image Cleaner to clean up stale images on your Azure Kubernetes Service cluster (preview)
-#az feature register --namespace "Microsoft.ContainerService" --name "EnableImageCleanerPreview"
-#It takes a few minutes for the status to show Registered. Verify the registration status by using the az feature show command:
-#az feature show --namespace "Microsoft.ContainerService" --name "EnableImageCleanerPreview"
-#When the status reflects Registered, refresh the registration of the Microsoft.ContainerService resource provider by using the az provider register command:
-#az provider register --namespace Microsoft.ContainerService
-
-az feature register --namespace Microsoft.Compute --name EncryptionAtHost
-az provider register -n Microsoft.Compute
+#!/bin/bash
 
 # Subscription id, subscription name, and tenant id of the current subscription
 subscriptionId=$(az account show --query id --output tsv)
 subscriptionName=$(az account show --query name --output tsv)
 tenantId=$(az account show --query tenantId --output tsv)
+
+
+
+# Install EncryptionAtHost Azure extension
+echo "Checking if [EncryptionAtHost] extension is already installed..."
+extension=$(az feature list -o table --query "[?contains(name, 'Microsoft.Compute/EncryptionAtHost') && @.properties.state == 'Registered'].{Name:name}" --output tsv)
+if [[ -z $extension ]]; then
+  echo "[EncryptionAtHost] extension is not installed. Installing..."
+  # Install aks-preview extension
+  az feature register --namespace Microsoft.Compute --name EncryptionAtHost
+  #Required to get the change propagated
+  else
+    echo "[EncryptionAtHost] extension is already registered."
+fi
+
+delay=1
+
+  echo -n "Checking if [EncryptionAtHost] extension is already registered..."
+  while true; do
+    extension=$(az feature list -o table --query "[?contains(name, 'Microsoft.Compute/EncryptionAtHost') && @.properties.state == 'Registered'].{Name:name}" --output tsv)
+    if [[ -z $extension ]]; then
+      echo -n "."
+      sleep $delay
+    else
+      echo ". DONE!"
+      echo "Invoking 'az provider register' is required to get the change propagated"
+      az provider register -n Microsoft.Compute
+      break
+    fi
+  done
 
 # Install aks-preview Azure extension
 echo "Checking if [aks-preview] extension is already installed..."
@@ -83,8 +84,40 @@ for aksExtension in ${registeringExtensions[@]}; do
       echo -n "."
       sleep $delay
     else
-      echo "."
+      echo ". DONE!"
+      echo "Invoking 'az provider register' is required to get the change propagated"
+      az provider register --namespace Microsoft.ContainerService
       break
     fi
   done
 done
+
+
+
+#AKS preview features are available on a self-service, opt-in basis. 
+#Previews are provided "as is" and "as available," and they're excluded from the service-level agreements and limited warranty. 
+#AKS previews are partially covered by customer support on a best-effort basis. As such, these features aren't meant for production use. 
+#For more information, see the following support articles:
+#https://learn.microsoft.com/en-us/azure/aks/vertical-pod-autoscaler
+#https://learn.microsoft.com/en-us/azure/aks/image-cleaner?tabs=azure-cli
+
+#To install the aks-preview extension, run the following command: Azure CLI
+#Open Cloudshell
+#az extension add --name aks-preview
+#Run the following command to update to the latest version of the extension released:
+#az extension update --name aks-preview
+
+#Register the 'AKS-VPAPreview' feature flag
+#Register the AKS-VPAPreview feature flag by using the az feature register command, as shown in the following example:
+#az feature register --namespace "Microsoft.ContainerService" --name "AKS-VPAPreview"
+#It takes a few minutes for the status to show Registered. Verify the registration status by using the az feature show command:
+#az feature show --namespace "Microsoft.ContainerService" --name "AKS-VPAPreview"
+#When the status reflects Registered, refresh the registration of the Microsoft.ContainerService resource provider by using the az provider register command:
+#az provider register --namespace Microsoft.ContainerService
+
+# Use Image Cleaner to clean up stale images on your Azure Kubernetes Service cluster (preview)
+#az feature register --namespace "Microsoft.ContainerService" --name "EnableImageCleanerPreview"
+#It takes a few minutes for the status to show Registered. Verify the registration status by using the az feature show command:
+#az feature show --namespace "Microsoft.ContainerService" --name "EnableImageCleanerPreview"
+#When the status reflects Registered, refresh the registration of the Microsoft.ContainerService resource provider by using the az provider register command:
+#az provider register --namespace Microsoft.ContainerService
