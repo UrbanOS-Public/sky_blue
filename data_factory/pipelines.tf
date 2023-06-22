23900034886209d5d21681b041e1807dcc365d3f
Marko Ristic <marko.ristic@avanade.com>
2023-06-22 10:31:46 -0400

    AIM

--- data_factory/pipelines.tf
+++ data_factory/pipelines.tf
@@ -1,6 +1,6 @@
 resource "azurerm_data_factory_pipeline" "pipeline_files" {
   for_each    = fileset("./pipelines", "*.json")
-  name = replace(each.key, ".josn", "")
+  name = replace(each.key, ".json", "")
   data_factory_id = azurerm_data_factory.adf.id
   activities_json = <<JSON
         ${(file("./pipelines/${each.key}"))}
