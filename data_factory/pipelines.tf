dbbf32a4edf6c45a776de0d77050767e5fa2de51
Marko Ristic <marko.ristic@avanade.com>
2023-06-22 10:05:33 -0400

    AIM

--- data_factory/pipelines.tf
+++ data_factory/pipelines.tf
@@ -2,7 +2,7 @@ locals {
   pipelines = { for value in fileset("./adf/pipelines", "*.json") : value => jsondecode(file("./adf/pipelines/${value}")) }
 }
 
-resource "azurerm_data_factory_pipeline" "data_transfer_crash" {
+resource "azurerm_data_factory_pipeline" "pipeline_files" {
   for_each = local.pipelines
   name            = each.value.name
   data_factory_id = azurerm_data_factory.adf.id
