4c1b9cd08fba5c1bedffe985119f5366fbebc4f1
Marko Ristic <marko.ristic@avanade.com>
2023-06-22 15:05:00 -0400

    AIM

--- data_factory/pipelines.tf
+++ data_factory/pipelines.tf
@@ -1,6 +1,6 @@
 resource "azurerm_data_factory_pipeline" "pipeline_files" {
-  for_each    = fileset("./pipelines", "*.json")
-  name = replace(each.key, ".json", "")
+  for_each        = fileset("./pipelines", "*.json")
+  name            = replace(each.key, ".json", "")
   data_factory_id = azurerm_data_factory.adf.id
   activities_json = <<JSON
         ${(file("./pipelines/${each.key}"))}
@@ -11,17 +11,19 @@ resource "azurerm_data_factory_pipeline" "pipeline_files" {
     azurerm_data_factory_custom_dataset.data,
     azurerm_data_factory_custom_dataset.rawstore
   ]
-
 }
 
 resource "azurerm_data_factory_trigger_schedule" "pipeline_files" {
-  for_each    = fileset("./pipelines", "*.json")
-  name = "tg-${(replace(each.key, ".json", ""))}"
+  for_each        = fileset("./pipelines", "*.json")
+  name            = "tg-${(replace(each.key, ".json", ""))}"
   data_factory_id = azurerm_data_factory.adf.id
   pipeline_name   = replace(each.key, ".json", "")
 
-  interval  = 1
-  frequency = "Day"
+  interval   = 1
+  frequency  = "Day"
+  start_time = "03:00"
+  time_zone  = "EST"
+ 
   depends_on = [ 
     azurerm_data_factory_pipeline.pipeline_files
   ]
