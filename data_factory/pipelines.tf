3d188ad886076b6ff8074fbc4cfdb286ed7717dc
Marko Ristic <marko.ristic@avanade.com>
2023-06-22 15:50:45 -0400

    AIM

--- data_factory/pipelines.tf
+++ data_factory/pipelines.tf
@@ -25,8 +25,8 @@ resource "azurerm_data_factory_trigger_schedule" "pipeline_files" {
   #time_zone  = "EST"
   
   schedule {
-    hours   = 3
-    minutes = 0
+    hours   = [3]
+    minutes = [0]
   }
 
   depends_on = [ 
