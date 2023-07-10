21730bb8986f99068aaec5417594f2b8b999fbd0
Marko Ristic <marko.ristic@avanade.com>
2023-07-10 14:22:56 -0400

    ADL 2

--- data_factory/data_flow.tf
+++ data_factory/data_flow.tf
@@ -23,7 +23,10 @@ resource "azurerm_data_factory_data_flow" "data_flow_files" {
     name = "flatten1"
   }
 
-  script_lines = [file("./data_flow/${each.key}.txt")]
+  script_lines = [<<EOT
+  file("./data_flow/${each.key}.txt")
+  EOT
+  ]
   
 
   depends_on = [  
