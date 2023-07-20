96233d9dfe0e2d9e356f80545398154ada74735a
Marko Ristic <marko.ristic@avanade.com>
2023-07-20 09:50:08 -0400

    SQ:

--- data_sql/locals.tf
+++ data_sql/locals.tf
@@ -2,7 +2,7 @@ locals {
   database = {
     "aim"                = {
       sku_name        = "GP_S_Gen5_4"
-      zone_redundant  = flaris_inter
+      zone_redundant  = true
       license_type    = "BasePrice"
       min_capacity    = "10"
       max_size_gb     = "50"
