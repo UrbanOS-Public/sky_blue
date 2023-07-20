6ef3fdc3eaad8655a2ccdeb8e6448aa4350bf116
Marko Ristic <marko.ristic@avanade.com>
2023-07-20 10:03:40 -0400

    SQL

--- data_sql/sql.tf
+++ data_sql/sql.tf
@@ -92,7 +92,7 @@ resource "azurerm_mssql_database" "aim" {
   name           = each.key
   server_id      = azurerm_mssql_server.aim.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = each.value.license_type
+  
   min_capacity   = each.value.min_capacity
   max_size_gb    = each.value.max_size_gb
   auto_pause_delay_in_minutes = each.value.auto_pause_delay_in_minutes
@@ -100,6 +100,7 @@ resource "azurerm_mssql_database" "aim" {
   sku_name       = each.value.sku_name
   #This property is only settable for Premium and Business Critical databases.
   #zone_redundant = each.value.zone_redundant
+  #license_type   = each.value.license_type
   #read_scale     = true
   geo_backup_enabled = true
   maintenance_configuration_name = "SQL_Default"
