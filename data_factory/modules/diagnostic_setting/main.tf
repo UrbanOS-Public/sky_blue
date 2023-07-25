a0b33e2d86e52d7fd64dfac5d5b2d3361f68c78a
Marko Ristic <marko.ristic@avanade.com>
2023-07-25 11:41:10 -0400

    SQL FW

--- data_factory/modules/diagnostic_setting/main.tf
+++ data_factory/modules/diagnostic_setting/main.tf
@@ -7,16 +7,16 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
   target_resource_id         = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
   log_analytics_destination_type = var.log_analytics_destination_type
-  dynamic "log" {
+  dynamic "enabled_log" {
     for_each = toset(data.azurerm_monitor_diagnostic_categories.this.logs)
 
     content {
-      category = log.value
-      enabled  = contains(var.logs,log.value) 
+      category = enabled_log.value
+      enabled  = contains(var.logs,enabled_log.value) 
 
       retention_policy {
-          enabled = contains(var.logs,log.value)
-          days    = contains(var.logs,log.value) == true ? var.retention_policy_days :0
+          enabled = contains(var.logs,enabled_log.value)
+          days    = contains(var.logs,enabled_log.value) == true ? var.retention_policy_days :0
       }
     }
   }
--- data_factory/modules/diagnostic_setting/outputs.tf
+++ data_factory/modules/diagnostic_setting/outputs.tf
@@ -2,3 +2,13 @@ output "id" {
   value = azurerm_monitor_diagnostic_setting.this.id
   description = "The ID of the Diagnostic Setting."
 }
+
+output "log_category_type" {
+  value = data.azurerm_monitor_diagnostic_categories.this.log_category_type
+  description = "The log_category_type of the Diagnostic Setting."
+}
+
+output "log_category_groups" {
+  value = data.azurerm_monitor_diagnostic_categories.this.log_category_groups
+  description = "The log_category_groups of the Diagnostic Setting."
+}
