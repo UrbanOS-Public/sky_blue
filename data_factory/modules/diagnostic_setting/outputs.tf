605d87f8599da90279aee61d011a564b5437c3fd
Marko Ristic <marko.ristic@avanade.com>
2023-07-25 10:52:41 -0400

    SQL FW

--- data_factory/modules/diagnostic_setting/outputs.tf
+++ data_factory/modules/diagnostic_setting/outputs.tf
@@ -1,9 +1,4 @@
-output "name" {
-  value = azurerm_key_vault.key_vault.name
-  description = "Specifies the name of the key vault."
-}
-
 output "id" {
-  value = azurerm_key_vault.key_vault.id
-  description = "Specifies the resource id of the key vault."
+  value = azurerm_monitor_diagnostic_setting.this.id
+  description = "The ID of the Diagnostic Setting."
 }
