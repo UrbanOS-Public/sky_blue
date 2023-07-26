fbfc6bab3bbbb0e2eb1ed9c8071a2677a455f46f
Marko Ristic <marko.ristic@avanade.com>
2023-07-26 09:28:57 -0400

    SQL FW

---
+++ 01_Foundations/bastion.tf
@@ -0,0 +1,10 @@
+module "bastion_host" {
+   source                       = "./modules/bastion_host"
+   name                         = module.namespoke.bastion_host.name
+   location                     = var.location
+   resource_group_name          = module.namespoke.resource_group.name
+   subnet_id                    = module.hub_network.subnet_ids["AzureBastionSubnet"] #data.azurerm_subnet.spoke_bastion_subnet.id 
+   log_analytics_workspace_id   = data.azurerm_log_analytics_workspace.law.id
+   log_analytics_retention_days = var.log_analytics_retention_days
+   tags = var.tags
+}
