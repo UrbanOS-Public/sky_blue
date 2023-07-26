e50950aaece3fa30e2949c67d59df414f108b2a1
Marko Ristic <marko.ristic@avanade.com>
2023-07-26 14:21:47 -0400

    SQL FW

--- databricks/databricks.tf
+++ databricks/databricks.tf
@@ -1,8 +1,8 @@
 #Create Resource Group for databricks
 resource "azurerm_resource_group" "databricks" {
-  name     = module.namepurview.resource_group.name
+  name     = module.nameadb.resource_group.name
   location = var.location
   tags     = var.tags
-  depends_on = [ module.namepurview ]
+  depends_on = [ module.nameadb ]
 }
 
