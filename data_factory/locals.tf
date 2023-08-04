6ca120151722759570782e699b6ce4c96903c811
Marko Ristic <marko.ristic@avanade.com>
2023-08-04 09:53:46 -0400

    ADF

--- data_factory/locals.tf
+++ data_factory/locals.tf
@@ -330,6 +330,10 @@ locals {
       folder = "adl/raw/weather"
       data_type  = "Json"
     }    
+    "adl_weather_data_json_all" = {
+      folder = "adl/raw/weather"
+      data_type  = "Json"
+    }   
     "adl_arcgis_boundingbox_json_all" = {
       folder = "adl/raw/arcgis"
       data_type  = "Json"
