050a4891508fa9e5b31c0afd9f0a652b17fd384e
Marko Ristic <marko.ristic@avanade.com>
2023-08-09 14:52:02 -0400

    adb

--- databricks_clusters/cluster.tf
+++ databricks_clusters/cluster.tf
@@ -1,6 +1,6 @@
 resource "databricks_cluster" "cluster_with_table_access_control" {
   cluster_name            = "Shared High-Concurrency"
-  spark_version           = data.databricks_spark_version.latest_lts.id
+  spark_version           = data.databricks_spark_version.latest.id
   node_type_id            = data.databricks_node_type.smallest.id
   autotermination_minutes = 20
 
