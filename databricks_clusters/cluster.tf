63f137fe5e236de86b6e4afc99790a5a78628fe6
Marko Ristic <marko.ristic@avanade.com>
2023-08-09 15:17:31 -0400

    adb

--- databricks_clusters/cluster.tf
+++ databricks_clusters/cluster.tf
@@ -1,10 +1,10 @@
-resource "databricks_cluster" "shared_autoscaling" {
-  cluster_name            = "Shared Autoscaling"
-  spark_version           = data.databricks_spark_version.latest_lts.id
-  node_type_id            = data.databricks_node_type.smallest.id
-  autotermination_minutes = 20
-  autoscale {
-    min_workers = 1
-    max_workers = 50
-  }
-}
+# resource "databricks_cluster" "shared_autoscaling" {
+#   cluster_name            = "Shared Autoscaling"
+#   spark_version           = data.databricks_spark_version.latest_lts.id
+#   node_type_id            = data.databricks_node_type.smallest.id
+#   autotermination_minutes = 20
+#   autoscale {
+#     min_workers = 1
+#     max_workers = 50
+#   }
+# }
