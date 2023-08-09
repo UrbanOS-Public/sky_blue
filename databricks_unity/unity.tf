aabf4bf08bf5df862f136040aacc1a01775b8c4e
Marko Ristic <marko.ristic@avanade.com>
2023-08-09 12:26:21 -0400

    adb

--- databricks_unity/unity.tf
+++ databricks_unity/unity.tf
@@ -6,18 +6,18 @@ resource "databricks_metastore" "this" {
   force_destroy = true
 }
 
-resource "databricks_metastore_data_access" "first" {
-  metastore_id = databricks_metastore.this.id
-  name         = "the-keys"
-  azure_managed_identity {
-    access_connector_id = data.azurerm_resources.azurerm_databricks_access_connector.id
-  }
+# resource "databricks_metastore_data_access" "first" {
+#   metastore_id = databricks_metastore.this.id
+#   name         = "the-keys"
+#   azure_managed_identity {
+#     access_connector_id = data.azurerm_resources.azurerm_databricks_access_connector.id
+#   }
 
-  is_default = true
-}
+#   is_default = true
+# }
 
-resource "databricks_metastore_assignment" "this" {
-  workspace_id         = data.azurerm_databricks_workspace.this.workspace_id
-  metastore_id         = databricks_metastore.this.id
-  default_catalog_name = "hive_metastore"
-}
+# resource "databricks_metastore_assignment" "this" {
+#   workspace_id         = data.azurerm_databricks_workspace.this.workspace_id
+#   metastore_id         = databricks_metastore.this.id
+#   default_catalog_name = "hive_metastore"
+# }
