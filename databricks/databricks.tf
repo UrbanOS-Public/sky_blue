1e78f0b1cc646ee2cca602ef8b139a16c243b5ab
Marko Ristic <marko.ristic@avanade.com>
2023-08-10 09:40:05 -0400

    adb

--- databricks/databricks.tf
+++ databricks/databricks.tf
@@ -86,18 +86,6 @@ resource "azurerm_key_vault_key" "cmkworkspace" {
 #   application_id = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
 # }
 
-resource "azurerm_key_vault_access_policy" "adb_workspace" {
-  key_vault_id = data.azurerm_resources.key_vault.resources[0].id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "b61b070e-25a8-4d6a-aea4-d3fd427a28b4" #!!!!to add appid automaticly    data.azuread_application.databricks.object_id #azurerm_databricks_workspace.dp_workspace.storage_account_identity.0.principal_id
-  key_permissions = [
-    "Get", "List", "Encrypt", "Decrypt", "WrapKey" ,"UnwrapKey"
-  ]
-  depends_on = [ 
-    #azurerm_databricks_workspace.dp_workspace,
-    azurerm_key_vault_key.cmk
-  ]
-}
 resource "azurerm_key_vault_access_policy" "adb_identity" {
   key_vault_id = data.azurerm_resources.key_vault.resources[0].id
   tenant_id    = data.azurerm_client_config.current.tenant_id
