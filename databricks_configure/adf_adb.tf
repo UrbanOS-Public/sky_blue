224a9dca416ac9c3accf6d2b7dda93bf06986b34
Marko Ristic <marko.ristic@avanade.com>
2023-08-14 13:54:39 -0400

    WIN

--- databricks_configure/adf_adb.tf
+++ databricks_configure/adf_adb.tf
@@ -12,3 +12,16 @@ resource "azurerm_data_factory_managed_private_endpoint" "adl_boundaries_data_js
   target_resource_id = data.azurerm_databricks_workspace.this.id
   subresource_name   = "databricks_ui_api"
 }
+
+resource "azurerm_data_factory_linked_service_azure_databricks" "msi_linked" {
+  name                       = module.nameadb.databricks_workspace.name
+  data_factory_id            = data.azurerm_data_factory.adf.id
+  description                = "ADB Linked Service via MSI"
+  adb_domain                 = "https://${data.azurerm_databricks_workspace.this.workspace_url}"
+  msi_work_space_resource_id = data.azurerm_databricks_workspace.this.id
+  integration_runtime_name   = "vnetRuntime"
+  existing_cluster_id        = databricks_cluster.this[0].id
+  depends_on = [ 
+    databricks_cluster.this
+  ]
+}
