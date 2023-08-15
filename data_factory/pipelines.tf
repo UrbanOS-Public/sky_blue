8b7343a0da2599317c11d91984cfde853ebfba20
Marko Ristic <marko.ristic@avanade.com>
2023-08-15 12:58:30 -0400

    WIN

--- data_factory/pipelines.tf
+++ data_factory/pipelines.tf
@@ -1,18 +1,18 @@
-# resource "azurerm_data_factory_pipeline" "pipeline_files" {
-#   for_each        = fileset("./pipelines", "*.json")
-#   name            = replace(each.key, ".json", "")
-#   data_factory_id = azurerm_data_factory.adf.id
-#   activities_json = <<JSON
-#         ${(file("./pipelines/${each.key}"))}
-#   JSON
+resource "azurerm_data_factory_pipeline" "pipeline_files" {
+  for_each        = fileset("./pipelines", "*.json")
+  name            = replace(each.key, ".json", "")
+  data_factory_id = azurerm_data_factory.adf.id
+  activities_json = <<JSON
+        ${(file("./pipelines/${each.key}"))}
+  JSON
 
-#   depends_on = [  
-#     azurerm_data_factory_linked_custom_service.adf,
-#     azurerm_data_factory_custom_dataset.data,
-#     azurerm_data_factory_custom_dataset.rawstore,
-#     azurerm_data_factory_custom_dataset.data_rest
-#   ]
-# }
+  depends_on = [  
+    azurerm_data_factory_linked_custom_service.adf,
+    azurerm_data_factory_custom_dataset.data,
+    azurerm_data_factory_custom_dataset.rawstore,
+    azurerm_data_factory_custom_dataset.data_rest
+  ]
+}
 
 # resource "azurerm_data_factory_trigger_schedule" "pipeline_files" {
 #   for_each        = fileset("./pipelines", "*.json")
@@ -36,43 +36,43 @@
 # }
 
 
-resource "azurerm_data_factory_pipeline" "pipeline_files" {
-  for_each = local.adf_pipelines
-  name            = each.key
-  data_factory_id = azurerm_data_factory.adf.id
-  description     = each.value.desc
-  folder          = each.value.folder
-  concurrency     = each.value.concurrency
-  activities_json = <<JSON
-        ${(file("./pipelines/${each.key}.json"))}
-  JSON
+# resource "azurerm_data_factory_pipeline" "pipeline_files" {
+#   for_each = local.adf_pipelines
+#   name            = each.key
+#   data_factory_id = azurerm_data_factory.adf.id
+#   description     = each.value.desc
+#   folder          = each.value.folder
+#   concurrency     = each.value.concurrency
+#   activities_json = <<JSON
+#         ${(file("./pipelines/${each.key}.json"))}
+#   JSON
 
-  depends_on = [  
-    azurerm_data_factory_linked_custom_service.adf,
-    azurerm_data_factory_custom_dataset.data,
-    azurerm_data_factory_custom_dataset.rawstore,
-    azurerm_data_factory_custom_dataset.data_rest
-  ]
-}
+#   depends_on = [  
+#     azurerm_data_factory_linked_custom_service.adf,
+#     azurerm_data_factory_custom_dataset.data,
+#     azurerm_data_factory_custom_dataset.rawstore,
+#     azurerm_data_factory_custom_dataset.data_rest
+#   ]
+# }
 
-resource "azurerm_data_factory_trigger_schedule" "pipeline_files" {
-  for_each = local.adf_pipelines
-  name            = "tg-${each.key}"
-  data_factory_id = azurerm_data_factory.adf.id
-  pipeline_name   = each.key
-  description     = "tg set by tf"
-  interval   = 1
-  frequency  = "Week"
-  #start_time = "03:00:00"
-  #time_zone  = "EST"
+# resource "azurerm_data_factory_trigger_schedule" "pipeline_files" {
+#   for_each = local.adf_pipelines
+#   name            = "tg-${each.key}"
+#   data_factory_id = azurerm_data_factory.adf.id
+#   pipeline_name   = each.key
+#   description     = "tg set by tf"
+#   interval   = 1
+#   frequency  = "Week"
+#   #start_time = "03:00:00"
+#   #time_zone  = "EST"
   
-  schedule {
-    days_of_week = ["Sunday"]
-    hours   = [3]
-    minutes = [0]
-  }
+#   schedule {
+#     days_of_week = ["Sunday"]
+#     hours   = [3]
+#     minutes = [0]
+#   }
 
-  depends_on = [ 
-    azurerm_data_factory_pipeline.pipeline_files
-  ]
-}
+#   depends_on = [ 
+#     azurerm_data_factory_pipeline.pipeline_files
+#   ]
+# }
